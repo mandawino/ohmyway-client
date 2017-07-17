c1014c579302fd4e449c6bf8933ddd12aae5e6ac
theo <mandawino@gmail.com>
2017-07-17 20:15:39 +0200

    Improve reducer with a switch case

--- src/reducers/Reducer.jsx
+++ src/reducers/Reducer.jsx
@@ -1,18 +1,25 @@
 const Reducer = (state = 0, action) => {
-    if(state == undefined){
-        console.log('undefined')
-        state=0;
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return state+1;
+        case 'DECREMENT':
+            return state-1;
+        default:
+            return state
     }
-    console.log(state, action);
-    if(action.type === 'INCREMENT'){
-        console.log('INCREMENT')
-        return state+1;
-    } else if (action.type === 'DECREMENT') {
-        console.log('DECREMENT')
-        return state-1;
-    } else {
-        return state;
-    }
+
+
+
+    // if(action.type === 'INCREMENT'){
+    //     console.log('INCREMENT')
+    //     return state+1;
+    // } else if (action.type === 'DECREMENT') {
+    //     console.log('DECREMENT')
+    //     return state-1;
+    // } else {
+    //     return state;
+    // }
 };
 
 export default Reducer;
