18240a16e9d4f85cbe51da9fec3067edee989846
Theo Guerin <mandawino@gmail.com>
2019-02-13 15:46:25 +0100

    Remove whatwg-fetch

--- config/webpack.js
+++ config/webpack.js
@@ -10,7 +10,6 @@ module.exports = {
         'webpack-dev-server/client?http://localhost:3000',
         'webpack/hot/only-dev-server',
         './node_modules/core-js/client/core.js',
-        'whatwg-fetch',
         'react-hot-loader/patch',
         path.resolve(__dirname, '../src/App')
     ],
