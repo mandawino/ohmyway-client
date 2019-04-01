8abba553f85bf4511a90ead13c50bb59d9c10245
Theo Guerin <mandawino@gmail.com>
2019-04-01 15:13:29 +0200

    Remane jsx files to js

--- src/containers/Stream.js
+++ src/containers/Stream.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Image from '../presenters/Image.jsx';
+import Image from '../presenters/Image';
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
